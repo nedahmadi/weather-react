8de2b78dbf192206b9fe066dbb5bce17d7d48fa2
nedahmadi <nedahmadi83@gmail.com>
2022-02-22 15:32:48 +0330

    Added source-code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,19 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weathersearch />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <span>
+          {" "}
+          <a href="https://github.com/nedahmadi/weather-react">
+            {" "}
+            Open-source code{" "}
+          </a>
+          ,by Neda Ahmadi{" "}
+        </span>
       </header>
     </div>
   );
